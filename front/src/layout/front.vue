9780181d9a2a5017a08082239b74eaee3dc20ea2
zhongjunsijia <2908204553@qq.com>
2025-07-07 23:48:27 +0800

    front change

--- front/src/layout/front.vue
+++ front/src/layout/front.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bakery-layout">
-    <!--    头部-->
+    <!-- 头部 -->
     <div class="bakery-header">
       <div class="bakery-container">
         <el-row type="flex" align="middle">
@@ -9,35 +9,28 @@
           </el-col>
           <el-col :span="10">
             <el-menu class="bakery-nav" :default-active="$route.path" @select="handleMenuSelect" mode="horizontal">
-              <el-menu-item index="/front/home">
-                首页
-              </el-menu-item>
-              <el-menu-item index="/front/cart">
-                购物车
-              </el-menu-item>
-              <el-menu-item index="/front/order">
-                我的订单
-              </el-menu-item>
-              <el-menu-item index="/front/collect">
-                收藏夹
-              </el-menu-item>
-              <el-menu-item index="/front/person">
-                个人中心
-              </el-menu-item>
-              <el-menu-item index="/front/address">
-                收货地址
-              </el-menu-item>
+              <el-menu-item index="/front/home">首页</el-menu-item>
+              <el-menu-item index="/front/cart">购物车</el-menu-item>
+              <el-menu-item index="/front/order">我的订单</el-menu-item>
+              <el-menu-item index="/front/collect">收藏夹</el-menu-item>
+              <el-menu-item index="/front/person">个人中心</el-menu-item>
+              <el-menu-item index="/front/address">收货地址</el-menu-item>
             </el-menu>
           </el-col>
-          <el-col :span="6">
+          <el-col :span="8" :offset="2" class="search-col">
             <div v-if="!$route.path.includes('/search')" class="search-box">
-              <el-input style="width: 80%" placeholder="请输入商品名称搜索商品" v-model="searchText"></el-input>
-              <el-button class="bakery-btn" style="margin-left: 5px" @click="search">搜索</el-button>
+              <el-input
+                  style="max-width: 250px; width: 100%; margin-right: 10px;"
+                  placeholder="请输入商品名称搜索商品"
+                  v-model="searchText"
+              ></el-input>
+              <el-button class="bakery-btn" @click="search">搜索</el-button>
             </div>
             <div v-else>
               <div style="width: 100px;height: 20px"></div>
             </div>
           </el-col>
+
           <el-col :span="4">
             <div style="text-align: right; padding-right: 10px" v-if="user.id">
               <el-dropdown style="float: right;" @command="handleCommand">
@@ -54,7 +47,6 @@
               <el-button class="bakery-btn-outline" @click="$router.replace('/login')">登录</el-button>
               <el-button class="bakery-btn" @click="$router.replace('/register')">注册</el-button>
             </div>
-
           </el-col>
         </el-row>
       </div>
@@ -65,7 +57,7 @@
       <router-view />
     </div>
 
-    <!--    底部-->
+    <!-- 底部 -->
     <div class="bakery-footer">
       <div class="bakery-container">
         <el-row :gutter="20">
@@ -105,7 +97,6 @@
 </template>
 
 <script>
-
 export default {
   name: "Front",
   data() {
@@ -128,7 +119,6 @@ export default {
       if (command === 'loginOut') {
         sessionStorage.removeItem("user")
         this.$router.replace('/login');
-
       }
     },
     handleMenuSelect(index) {
@@ -160,7 +150,10 @@ export default {
   font-weight: 500;
 }
 
-/* 修复搜索框样式 */
+.search-col {
+  padding-left: 20px;
+}
+
 .search-box {
   height: 40px;
   padding: 5px 0;
